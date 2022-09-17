793024b7d308cc509bf3ca2e1475b5c62204a101
Fateh Singh <fateh288@gmail.com>
2022-09-17 18:53:24 +0000

    image upload to s3

--- web_tier/controller.py
+++ web_tier/controller.py
@@ -1,7 +1,11 @@
+import os
 from flask import Flask, request
 import boto3
 import numpy as np
 import sys
+
+os.environ['AWS_PROFILE'] = "baadal"
+os.environ['AWS_DEFAULT_REGION'] = "us-east-1"
 
 app = Flask(__name__)
 
@@ -14,8 +18,8 @@ def classify_image():
     f = request.files['myfile']
     filename = f.filename 
     print(filename, file=sys.stderr)
-
     client = boto3.client('s3', region_name='us-east-1')
-
-    client.upload_file('images/image_0.jpg', 'mybucket', 'image_0.jpg')
+    client.upload_fileobj(f, 'imagesbucketcse546', filename)
+    
+    
     return filename
